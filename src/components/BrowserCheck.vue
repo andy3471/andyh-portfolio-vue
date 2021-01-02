25ad87e822bc79d35c89c2f754fcf84bd4cbde89
Andrew Hargrave <me@andyh.app>
2021-01-02 15:50:10 +0000

    Install Vue

---
+++ src/components/BrowserCheck.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="columns is-mobile is-centered">
+    <div class="column is-half">
+      <div v-if="this.isIE" class="notification is-danger browser-notifcation">
+        This website does not support Internet Explorer, please try again on
+        <a href="https://www.mozilla.org/en-GB/firefox/new/">Firefox</a> or
+        <a href="https://www.google.co.uk/chrome/">Chrome</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isIE: false
+    };
+  },
+  mounted: function() {
+    var ua = navigator.userAgent;
+    var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+    this.isIE = is_ie;
+  }
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div
+      class="hamburger"
+      @click="isOpen = !isOpen"
+      v-bind:class="{ active: isOpen }"
+    >
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+    </div>
+    <section
+      class="nav-page hero is-fullheight"
+      v-bind:class="{ active: isOpen }"
+    >
+      <div class="nav-list columns is-desktop">
+        <div
+          v-for="(link, index) in links"
+          class="column nav-item"
+          v-bind:key="index"
+          @click="openLink()"
+        >
+          <a :href="link.href">{{ link.title }}</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+      links: [
+        { href: "#about", title: "About Me" },
+        { href: "#keys", title: "KeyShare" },
+        { href: "#rota", title: "LunchRota" },
+        { href: "#components", title: "Components" },
+        { href: "#hosting", title: "Hosting" },
+        { href: "#projects", title: "Other Projects" },
+        { href: "#work", title: "Work Experience" },
+        { href: "#contact", title: "Contact Me" }
+      ]
+    };
+  },
+  methods: {
+    openLink() {
+      this.isOpen = false;
+    }
+  }
+};
+</script>
